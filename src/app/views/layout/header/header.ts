7f02da3c09b7b55612312da002bd0ee3d224a669
vince.h <v.harmegnies@gmail.com>
2026-03-15 20:47:10 +0100

    change + routing header.html

--- src/app/views/layout/header/header.ts
+++ src/app/views/layout/header/header.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    MatIcon
+    MatIcon,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
